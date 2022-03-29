64e11682f9bf44d91b4cce2914849f1f42bc45e7
Thomas Buckley <tombuckley91@gmail.com>
2022-03-29 18:53:55 +0100

    Initial Routes for students & pets

---
+++ controllers/api/students/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+// /api/students
+
+router.get("/", (req, res) => {
+  // Grab all students
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const router = require("./controllers");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(router);
 
 app.listen(() => {
   console.log(`App listening on port: ${PORT}`);
